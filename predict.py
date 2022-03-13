2c6bd60922b0a60864fdb8b313ed08cd5d12aebc
Bishnu Tamang <bishnut219@gmail.com>
2022-03-13 10:34:14 +0545

    my first commit

---
+++ predict.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
+model = tf.keras.models.load_model(filepath = "model.h5")
+
+import numpy as np
+
+class_names= ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+
+IMAGE_SIZE = 256
+BATCH_SIZE = 32
+CHANNELS = 3
+EPOCHS = 50
+
+def detect():
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(
+        "C:\\Users\\User\\Desktop\\1note\\potato",
+        shuffle = True,
+        image_size = (IMAGE_SIZE, IMAGE_SIZE),
+        batch_size = BATCH_SIZE)
+
+    for images, labels in dataset:
+        print(type(images))
+        img_array = tf.keras.preprocessing.image.img_to_array(images[0].numpy())
+        img_array = tf.expand_dims(img_array,0)
+
+        predictions = model.predict(img_array)
+        predicted_class = class_names[np.argmax(predictions[0])]
+        confidence = round(100 * (np.max(predictions[0])),2)
+        
+        print(predicted_class)
+        print(confidence)
+        return predicted_class, confidence
